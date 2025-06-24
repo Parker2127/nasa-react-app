a9c5e57ad910c5d136230e16e85b35aedd1fb2e2
Parker2127 <shrikarudayarcs21@gmail.com>
2025-06-24 06:44:07 -0400

    Added useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 
 function App() {
 
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <>
     <Main />
-    <SideBar/>
+    { showModal && (
+      
+      <SideBar/>
+
+    )}
     <Footer />
     </>
   )

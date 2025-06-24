2413cad3ffca498db33473628850431e62240b52
Parker2127 <shrikarudayarcs21@gmail.com>
2025-06-23 21:55:37 -0400

    Styling the sidebar-2

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,15 +1,15 @@
 
 export default function SideBar() {
   return (
-        <div className ="sidebarContents">
-            <h2> Martian Landscape</h2>
-
-            <div>
-                <p> Description</p>
-                <p> ioasfiuahfiugairsbfguiarhgipuabvouiasebviuadhrgvpiuaerbvipaeurrvbjkaerbveb</p>
-            </div>
-
-            
+        <div className="sidebar">
+          <div className="bgOverlay"></div>
+          <div className="sidebarContents">
+              <h2> Martian Landscape</h2>
+              <div>
+                  <p> Description</p>
+                  <p> ioasfiuahfiugairsbfguiarhgipuabvouiasebviuadhrgvpiuaerbvipaeurrvbjkaerbveb</p>
+              </div>
+          </div>
         </div>
   )
 }
